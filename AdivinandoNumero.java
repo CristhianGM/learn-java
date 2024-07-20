6b2a65c7a8013532b3927e23686ec1caee666566
Cristhian <122185342+CristhianGM@users.noreply.github.com>
2024-07-20 11:11:41 -0500

    Add files via upload

---
+++ AdivinandoNumero.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class AdivinandoNumero {
+    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            testearAdivinandoNumero();
+        }
+    }
+
+    public static void testearAdivinandoNumero() {
+        int min = 1;
+        int max = 20;
+        int numeroAdivinar = numeroAleatorio(min, max);
+        Scanner scanner = new Scanner(System.in);
+        int numeroIngresado;
+        
+        System.out.println("Adivina un número entre 1 Y 20:");
+        
+        do {
+            numeroIngresado = scanner.nextInt();
+            if (numeroIngresado < numeroAdivinar) {
+                System.out.println("El número es mayor. Inténtalo de nuevo:");
+            } else if (numeroIngresado > numeroAdivinar) {
+                System.out.println("El número es menor. Inténtalo de nuevo:");
+            }
+        } while (numeroIngresado != numeroAdivinar);
+
+        System.out.println("¡Correcto! Has adivinado el número " + numeroAdivinar);
+        scanner.close();
+    }
+
+    public static int numeroAleatorio(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt((max - min) + 1) + min;
+    }
+}
