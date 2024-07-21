1ed7636dd3f1abe5cd087e886f10664be5d97214
Cristhian <122185342+CristhianGM@users.noreply.github.com>
2024-07-20 21:05:57 -0500

    Creando la  SumaDeNumeros.java
    Esta es una simple suma de numeros para un ejemplo

---
+++ SumaDeNumeros.java
@@ -0,0 +1,8 @@
+public class SumaDeNumeros {
+  public static void main (String args[]){
+    int n1 = 2;
+    int n2 = 3;
+    int resultado = n1+n2;
+    System.out.println("La suma es: "+ resultado);
+  }
+}
